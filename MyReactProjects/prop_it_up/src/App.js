7032233aee82c83c1dca714344fb6541edcffa6d
MRea <rotavele666@pm.me>
2021-07-07 21:53:49 -0700

    PuttingitTogether commit

--- MyReactProjects/prop_it_up/src/App.js
+++ MyReactProjects/prop_it_up/src/App.js
@@ -6,12 +6,13 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {"45"} hairColor = {"Black"}></PersonCard>
-      <PersonCard firstName = {"John"} lastName = {"Smith"} age = {"88"} hairColor = {"Brown"}></PersonCard>
-      <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {"50"} hairColor = {"Brown"}></PersonCard>
-      <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {"62"} hairColor = {"Brown"}></PersonCard>
+      <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {45} hairColor = {"Black"}></PersonCard>
+      <PersonCard firstName = {"John"} lastName = {"Smith"} age = {88} hairColor = {"Brown"}></PersonCard>
+      <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {50} hairColor = {"Brown"}></PersonCard>
+      <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {"Brown"}></PersonCard>
     </div>
   );
 }
 
 export default App;
+
--- MyReactProjects/prop_it_up/src/components/PersonCard.js
+++ MyReactProjects/prop_it_up/src/components/PersonCard.js
@@ -2,18 +2,22 @@
 import React,{Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
     render(){
-        const {firstName, lastName, age, hairColor } = this.props;
         return(
             <div>
-                <h1>{lastName},{firstName}</h1>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
+                <h1>{this.props.lastName},{this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick= { () => this.setState({ age: this.state.age +1 }) }>Birthday Button for {this.props.firstName}</button>
             </div>
         );
     }
-
 }
-
 
 export default PersonCard;
