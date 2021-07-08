58c8f62853a320bd7c4778e212f4d87ea62247a0
MRea <rotavele666@pm.me>
2021-07-07 21:58:51 -0700

    needless commit

---
+++ MyReactProjects/putting_it_together/src/components/PersonCard.js
@@ -0,0 +1,23 @@
+
+import React,{Component} from 'react';
+
+class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.props.lastName},{this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick= { () => this.setState({ age: this.state.age +1 }) }>Birthday Button for {this.props.firstName}</button>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
