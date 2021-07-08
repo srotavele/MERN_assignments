32fb8e52e94ce1bca7bb8665cb898ddf65a42a22
MRea <rotavele666@pm.me>
2021-07-07 17:49:58 -0700

    Initial commit

---
+++ MyReactProjects/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+        <ul class="short_list">
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run A Marathon</li>
+          <li>Feed the Dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
